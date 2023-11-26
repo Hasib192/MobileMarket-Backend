b8d449a304d96cef8dd65807aa8b15ba6538ad91
Hasib <hasibhosen7612@gmail.com>
2023-11-26 08:25:01 +0600

    fix/product-model

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,12 +6,10 @@ const productSchema = new Schema(
     title: String,
     short_des: String,
     price: Number,
-    discount: Number,
     discount_price: Number,
     image: String,
     stock: String,
     star: String,
-    remark: String,
   },
   { timestamps: true, versionKey: false }
 );
