5a3109afc0cedfbbb7478af7b3b2e85bf7f35383
Hasib <hasibhosen7612@gmail.com>
2023-12-26 07:20:03 +0600

    update/cart-controller

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,4 +1,5 @@
 const cartModel = require("../models/cartModel");
+const mongoose = require("mongoose");
 
 exports.createCart = async (req, res) => {
   try {
@@ -17,9 +18,25 @@ exports.createCart = async (req, res) => {
 
 exports.cartList = async (req, res) => {
   try {
-    let email = req.user.email;
+    let userId = new mongoose.Types.ObjectId(req.user._id);
 
-    
+    let result = await cartModel.aggregate([
+      { $match: { user: userId } },
+      {
+        $lookup: {
+          from: "products",
+          localField: "products",
+          foreignField: "_id",
+          as: "selected_products",
+        },
+      },
+      {
+        $project: {
+          selected_products: 1,
+        },
+      },
+    ]);
+    res.status(200).json({ status: true, data: result });
   } catch (error) {
     res.status(200).json({ status: false, data: error.message });
   }
